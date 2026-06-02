ca86ded80407167a84f6440cbf7b1745df7af52e
uysmn <ysmin0530@gmail.com>
2026-06-02 12:58:10 +0900

    Add files via upload

---
+++ readforest/src/main/java/com/readforest/readforest/ReadforestApplication.java
@@ -0,0 +1,13 @@
+package com.readforest.readforest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadforestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadforestApplication.class, args);
+	}
+
+}
